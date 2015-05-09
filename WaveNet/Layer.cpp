25167ccf4d2a601318454d9b80f888498af2b467
Daniel <xd009642@reading.ac.uk>
2015-05-09 14:41:33 +0100

    Added feedforward processing to layer

---
+++ WaveNet/Layer.cpp
@@ -0,0 +1,27 @@
+#include "layer.h"
+
+
+
+wnn::Layer::Layer(int in, int out):
+wavelons(out, wnn::Wavelon(in)),
+weights(in*out)
+{
+
+}
+
+std::vector<double> wnn::Layer::process(std::vector<double> in)
+{
+	std::vector<double> res;
+	int dim = weights.size() / wavelons.size();
+	std::vector<double> wavelonInput;
+	wavelonInput.resize(dim);
+	for (int i = 0; i < wavelons.size(); i++)
+	{
+		for (int j = 0; j < dim; j++)
+		{
+			wavelonInput[j]=(weights[i*dim + j] * in[j]);
+		}
+		res.push_back(wavelons[i].process(wavelonInput));
+	}
+	return res;
+}
--- WaveNet/layer.h
+++ WaveNet/layer.h
@@ -1,13 +1,15 @@
 #pragma once
 
 #include "Wavelon.h"
-
+#include <vector>
 
 namespace wnn
 {
 	class Layer
 	{
-
+	public:
+		Layer(int in, int out);
+		std::vector<double> process(std::vector<double> in);
 	private:
 		std::vector<Wavelon> wavelons;
 		std::vector<double> weights;

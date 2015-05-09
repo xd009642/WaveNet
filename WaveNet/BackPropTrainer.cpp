27d850f236fb4e61ec7b456bac432da2099bf597
Daniel <xd009642@reading.ac.uk>
2015-05-09 23:25:26 +0100

    Minor fix to start of BP
    Tweaked computation of error value to be correct
    Added variables for learning rate and momentum
    
    Will need to implement openCV next and stop using std::vector<double> as
    input. Or Armadillo....

--- WaveNet/BackPropTrainer.cpp
+++ WaveNet/BackPropTrainer.cpp
@@ -4,6 +4,8 @@
 
 void wnn::BackPropTrainer::train(Network& network, std::vector<double> data, double label)
 {
+	double n = 0.2, m = 0.4;
 	double actual=network.process(data);
-	double error = 0.5* pow(actual - label, 2);
+	double error = 0.5* pow(label - actual, 2);
+
 }
